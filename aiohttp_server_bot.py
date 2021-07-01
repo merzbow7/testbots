fc13dfb0af7d6ab74c7bed9628c48db90040d0cc
sevenamber <sevenamber@gmail.com>
2021-07-02 02:04:02 +0300

    add app factory

--- aiohttp_server_bot.py
+++ aiohttp_server_bot.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiohttp import web
 from aiohttp.web_request import BaseRequest
 from aiohttp import ClientSession
@@ -7,20 +9,19 @@ import asyncio
 
 
 async def post_handler(request: BaseRequest):
-    response = await request.json()
-    data = {"status": "ok"}
-    await send_message(response)
+    if request.body_exists:
+        response = await request.json()
+        await send_message(response)
+        data = {"status": "ok"}
+    else:
+        logging.warning("Incorrect request")
+        data = {"status": "error"}
     return web.json_response(data)
 
 
 async def send_message(response):
-    if not hasattr(app, "send_queue"):
-        app.send_queue = asyncio.Queue()
-        print('create queue')
-    if not hasattr(app, "send_task"):
-        app.send_task = asyncio.create_task(send_worker(app.send_queue))
-        print('create workers')
-    app.send_queue.put_nowait(response)
+    app.setdefault("send_task", asyncio.create_task(send_worker(app["send_queue"])))
+    app["send_queue"].put_nowait(response)
 
 
 async def send_worker(from_queue: asyncio.Queue, workers: int = 4):
@@ -43,15 +44,23 @@ async def sub_send_worker(session: ClientSession, from_queue: asyncio.Queue, cur
             elif text.upper()[1:] in currency_dict:
                 text = currency_dict[text.upper()[1:]]
             params = {'chat_id': chat_id, 'text': text}
-            if all(params.values()):
-                async with session.get(url, params=params) as response:
-                    if response.status != 200:
-                        print(await response.json())
+            async with session.get(url, params=params) as response:
+                if response.status != 200:
+                    print(await response.json())
+
+        else:
+            logging.warning("Incorrect request")
         from_queue.task_done()
 
 
-app = web.Application()
-app.add_routes([web.post(Config.post_handler, post_handler)])
+def create_app():
+    current_app = web.Application()
+    current_app.add_routes([web.post(Config.post_handler, post_handler)])
+    current_app["send_queue"] = asyncio.Queue()
+    return current_app
+
+
+app = create_app()
 
 if __name__ == '__main__':
     web.run_app(app)
